59b90f1a9e596d55cc7339f5ad8dcc06d7aff5ff
Joan Oliva <joliva@oneboxtm.com>
2016-02-19 12:29:04 +0100

    added response json headers

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Find specifications at:
 + [github](https://github.com/joliva-ob/onebox-dynamic-pricing-api)
 + [onebox-developer](http://developer.oneboxtickets.com/dynamic-pricing-api)
 
+TODO list
++ filters
++ errors
++ rehuse mysql connections / pool
++ catch mysql responses, coger informacion de DAL-mysql o de elasticsearch o MS de prices
--- dynamic-pricing.go
+++ dynamic-pricing.go
@@ -21,17 +21,20 @@ var date_to string = "2015-02-01"
 var limit int = 10
 var config configuration.Config
 
+
 /**
  * Main command to load configuration by given environment argument
  * and start application server to listen the exposed endpoints and
  * provide the requested resources operations
+ *
+ * Mandatory parameters are path (/tmp...) and environment (dev, qa, pre, pro...)
  */
 func main() {
 
 	// Load configuration to start application
-	var env = "./resources/" + os.Args[1] + ".yml"
-	config = configuration.LoadConfiguration(env)
-	log.Printf("--> dynamic-pricing started with environment: %s\n", os.Args[1])
+	var filename = os.Args[1] + "/" + os.Args[2] + ".yml"
+	config = configuration.LoadConfiguration(filename)
+	log.Printf("--> dynamic-pricing started with environment: %s\n", os.Args[2])
 
 	// Create the router to handle mockup requests with its response properly
 	router := mux.NewRouter().StrictSlash(true)
@@ -51,12 +54,16 @@ func pricesController(w http.ResponseWriter, r *http.Request) {
 
 	ms := time.Now().UnixNano()%1e6/1e3
 
+	// Retrieve requested resource information
 	prices := dataservice.GetPrices(date_from, date_to, limit, config)
 	pricesjson, err := json.Marshal(prices)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	// Set response headers
+	w.Header().Set("Content-Type", "application/json")
 
 	// Response string
 	fmt.Fprintln(w, string(pricesjson))
